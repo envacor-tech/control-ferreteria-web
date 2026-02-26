2a51c9eb146d958e8c6ac19236924bd5695a5ca3
envacor-tech <envacor@gmail.com>
2026-02-26 20:17:27 -0300

    Fix templates folder

--- app.py
+++ app.py
@@ -95,5 +95,8 @@ def historial():
     conn.close()
     return render_template("historial.html", pedidos=pedidos)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
